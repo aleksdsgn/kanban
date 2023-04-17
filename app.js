fa79ba06f72510b82352645c6e50c8c95ff1bec0
Aleksey Shlyapnikov <aleksdsgn@ya.ru>
2023-04-17 14:08:35 +0700

    feat: create script for placeholders

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const item = document.querySelector('.item');
+const placeholders = document.querySelectorAll('.placeholder');
 
 item.addEventListener('dragstart', dragstart);
 item.addEventListener('dragend', dragend);
+
+for (const placeholder of placeholders) {
+  // console.log(placeholder);
+  // когда перетаскиваемый элемент находится над плейсхолдером
+  placeholder.addEventListener('dragover', dragover)
+  // когда мы заходим на территорию плейсхолдера
+  placeholder.addEventListener('dragenter', dragenter)
+  // когда перетащили и вышли с территории плейсхолдера
+  placeholder.addEventListener('dragleave', dragleave)
+  // когда мы отпустили
+  placeholder.addEventListener('drop', dragdrop)
+}
 
 function dragstart(event) {
   // console.log('dragstart', event.target);
@@ -15,4 +28,28 @@ function dragend(event) {
   // event.target.classList.remove('hold', 'hide');
   // перетереть все классы и добавить только один
   event.target.className = 'item';
+}
+
+// когда перетаскиваемый элемент находится над плейсхолдером
+function dragover(event) {
+  event.preventDefault();
+  // console.log('drag over');
+}
+
+// когда мы заходим на территорию плейсхолдера
+function dragenter(event) {
+  event.target.classList.add('hovered');
+  console.log('drag enter');
+}
+
+// когда перетащили и вышли с территории плейсхолдера
+function dragleave(event) {
+  event.target.classList.remove('hovered');
+  // console.log('drag leave');
+}
+
+// когда мы отпустили
+function dragdrop(event) {
+  // console.log('drag drop');
+  event.target.append(item);
 }
